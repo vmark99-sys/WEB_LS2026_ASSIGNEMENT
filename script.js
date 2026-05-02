863cddcb0b75556b3bc2ec5ae11e27d53c7d8f98
Mark Varjasi <vmark99@gmail.com>
2026-05-03 00:42:35 +0200

    WIP: contact validation and designe
    Added all fields validation, email validation (email must include . and @)
    Message has to be at least 10 characters
    
    Added style to match more the page colors

--- script.js
+++ script.js
@@ -28,3 +28,70 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+
+//Forms validate
+function validateForm() {
+    let fname = document.forms["trainingForm"]["fname"].value.trim();
+    let lname = document.forms["trainingForm"]["lname"].value.trim();
+    let email = document.forms["trainingForm"]["email"].value.trim();
+    let dogname = document.forms["trainingForm"]["dogname"].value.trim();
+    let message = document.forms["trainingForm"]["message"].value.trim();
+
+    let trainingLevel = document.querySelector('input[name="training_level"]:checked');
+
+    let interests = document.querySelectorAll(
+        'input[name="interest_basic"]:checked, input[name="interest_tracking"]:checked, input[name="interest_protection"]:checked, input[name="interest_puppy"]:checked'
+    );
+
+
+    if (fname == "") {
+        alert("First name must be filled out");
+        return false;
+    }
+
+    if (lname == "") {
+        alert("Last name must be filled out");
+        return false;
+    }
+
+    if (email == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+
+    if (!email.includes("@") || !email.includes(".")) {
+        alert("Please enter a valid email address.");
+        return false;
+    }
+
+    if (dogname == "") {
+        alert("Dogs name must be filled out");
+        return false;
+    }
+
+
+    if (!trainingLevel) {
+        alert("Please choose a training level.");
+        return false;
+    }
+
+    if (interests.length === 0) {
+        alert("Please choose at least one interest.");
+        return false;
+    }
+
+    if (message == "") {
+        alert("Message must be filled out");
+        return false;
+    }
+
+    if (message.length < 10) {
+        alert("Message must be at least 10 characters long.");
+        return false;
+    }
+
+    alert("Form submitted successfully!");
+    return true;
+
+}
